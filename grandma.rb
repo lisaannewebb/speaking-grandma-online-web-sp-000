a98af147227ec34e62cb33e1733defffe8637c50
Lisa Webb <lwebbnyc@outlook.com>
2019-03-04 17:49:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,11 +17,11 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase == phrase.downcase
-    "HUH?! SPEAK UP, SONNY!"
-    
-  elsif phrase == phrase.upcase
+    elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+    
+    elsif phrase == phrase.downcase
+    "HUH?! SPEAK UP, SONNY!"
  
   end
 end
