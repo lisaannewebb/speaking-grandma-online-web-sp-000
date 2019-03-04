1b5ea790f6f35a19d8041f8ad363e4cd8fbd5f6f
Lisa Webb <lwebbnyc@outlook.com>
2019-03-04 17:43:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(speak)
   if speak == speak.upcase
     "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN!"
   else 
     "HUH?! SPEAK UP, SONNY!"
   end
