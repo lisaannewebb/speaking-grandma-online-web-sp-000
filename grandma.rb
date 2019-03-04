8acb0f0a6eb62df41cd93b1cf280a4ac9b769a6c
Lisa Webb <lwebbnyc@outlook.com>
2019-03-04 17:49:24 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(phrase)
     elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
     
-    elsif phrase == phrase.downcase
+    else phrase == phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
  
   end
